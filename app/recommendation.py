c9b4af2cf032c92dd7566e463b9736b02d0260c2
Chang-soo, Han <are@spoqa.com>
2016-07-16 03:17:48 +0900

    hello

--- app/recommendation.py
+++ app/recommendation.py
@@ -1,4 +1,9 @@
+import os
+
 from flask import Blueprint, redirect, render_template, request, url_for
+from werkzeug.utils import secure_filename
+
+from app.naver import parse
 
 recommendation = Blueprint('recommendation', __name__,
                            url_prefix='/recommendation')
@@ -128,49 +133,53 @@ def usage_of_each_category():
 
 @recommendation.route('/usage-pattern/')
 def usage_pattern():
+    return render_template('recommendation/usage-pattern/upload-xlsx.html')
+
+
+@recommendation.route('/calculate-usage-pattern/', methods=['POST'])
+def calculate_usage_pattern():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return redirect(request.referrer)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return redirect(request.referrer)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('./', filename))
+            patterns = parse(filename)
+            return render_template(
+                'recommendation/usage-pattern/calculated.html',
+                patterns=patterns
+            )
     return ''
+
+
 @recommendation.route('/result/')
 def result():
     return redirect(url_for('.recommendation_result'))
 
-@recommendation.route('/result/recommendation_result/')
-def recommendation_result():
-    recommendation_cards = [
-	{
-		'image': {
- 			'url' :url_for('static', filename='images/bg_maincard.png')},
-			'hashtag': '#영화 #편의점 #교통 #커피',
-			'name': '삼성카드6 V2',
-			'total_benefit': '28500',
-			'benefits': [{'benefit': '영화', 'price': '10000'}, 
-					{'benefit': '편의점', 'price': '12000'},
-					{'benefit': '교통', 'price': '30000'},
-					{'benefit': '커피', 'price': '20000'}]
-	},
-        {
-                'image': {
-                        'url' :url_for('static', filename='images/bg_nextcard.png')},
-                        'hashtag': '#영화 #편의점 #온라인쇼핑 #커피',
-                        'name': '삼성카드3',
-                        'total_benefit': '31500',
-                        'benefits': [{'benefit': '영화', 'price': '10000'}, 
-                                        {'benefit': '편의점', 'price': '22000'},
-                                        {'benefit': '온라인쇼핑', 'price': '50000'},
-                                        {'benefit': '커피', 'price': '20000'}]
-        },
-        {
-                'image': {
-                        'url' :url_for('static', filename='images/bg_maincard.png')},
-                        'hashtag': '#영화 #항공  #교통 #통신사',
-                        'name': '삼성카드5 V3',
-                        'total_benefit': '48500',
-                        'benefits': [{'benefit': '영화', 'price': '10000'}, 
-                                        {'benefit': '항공', 'price': '12000'},
-                                        {'benefit': '교통', 'price': '30000'},
-                                        {'benefit': '통신사', 'price': '20000'}]
-        }]
 
-    return render_template('recommendation/result/recommendation-result2.html', recommendation_cards=recommendation_cards)
+@recommendation.route('/result/recommendation_result/', methods=['POST'])
+def recommendation_result():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return redirect(request.referrer)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return redirect(request.referrer)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('./', filename))
+            recommendation_cards = parse(filename)
+            return render_template('recommendation/result/recommendation-result2.html', recommendation_cards=recommendation_cards)
+
 
 @recommendation.route('/result/recommendation-result-detail')
 def recommendation_result_detail():
